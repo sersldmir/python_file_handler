ade2431afcae7303b648988a4a5ca7dd65523e19
sersldmir <2sergmir2@gmail.com>
2022-06-29 15:05:17 +0300

    Add existing file

---
+++ file_manager_ver1.py
@@ -0,0 +1,4 @@
+'''File handler project. Capabilities:
+- working with csv files and perfoming some operations with them (add, divide, subtract, multiply, compare columns; concatenating, splitting and merging) 
+- pickling and unpickling files
+- working with text files'''
