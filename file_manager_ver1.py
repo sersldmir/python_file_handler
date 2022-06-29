5a0178d6233060dd5addda7736fe3c0c9ddd05fc
sersldmir <2sergmir2@gmail.com>
2022-06-29 16:41:34 +0300

    first line of code/test commit

--- file_manager_ver1.py
+++ file_manager_ver1.py
@@ -2,3 +2,5 @@
 - working with csv files and perfoming some operations with them (add, divide, subtract, multiply, compare columns; concatenating, splitting and merging) 
 - pickling and unpickling files
 - working with text files'''
+
+import csv, pickle
