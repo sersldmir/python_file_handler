df77348a7af04e85e6ea65180a2cb0ec707197e7
sersldmir <2sergmir2@gmail.com>
2022-07-03 22:29:47 +0300

    Version 0.2

--- file_manager_ver1.py
+++ file_manager_ver1.py
@@ -4,18 +4,61 @@ Capabilities:
 - pickling and unpickling files
 - working with text files'''
 
-import csv, pickle
+# import csv, pickle
 import pandas as pd
 import numpy as np
 from time import sleep
 
+# Custom class to work with data
 class My_data_class(object):
-    def __init__(self, data):
+    def __init__(self, data, name):
         self.rows=data
         self.columns=[[data[j][i] for j in range(len(data))] for i in range(len(data[0]))]
         self.main_data=pd.DataFrame({k: v for k,v in zip(data[0],[[data[j][i] for j in range(1,len(data))] for i in range(len(data[0]))])})
+        self.name=name
 
 
+    def __repr__(self):
+        return self.main_data.to_string(index=False)
+
+    # method for saving data in different formats, apart from .txt
+    @staticmethod
+    def regular_save(data, format, file_name):
+        try:
+            if format == 'csv':
+                data.to_csv(file_name +'.csv', index = False)
+                print(f'Data successfully saved in csv file under the name: {file_name + ".csv"}!')
+            if format == 'pickle':
+                data.to_pickle(file_name +'.pkl')
+                print(f'Data successfully pickled under the name: {file_name + ".pkl"}!')
+        except OSError:
+            raise ValueError('Invalid file name!')
+
+    # method for saving data in .txt
+    @staticmethod
+    def regular_txt_save(data, file_name):
+        try:
+            with open(file_name, mode='w') as file:
+                for line in data:
+                    file.write('\t'.join([str(i) for i in line])+'\n')
+            print(f'Data successfully saved in txt under the name {file_name+".txt"}!')
+        except OSError:
+            raise ValueError('Invalid file name!')
+    
+    def save_table(self, format='csv', max_rows=0):
+        if max_rows < 0:
+            max_rows = 0
+        elif max_rows == 0:
+            if format != 'txt':
+                My_data_class.regular_save(self.main_data, format, self.name)
+            else:
+                My_data_class.regular_txt_save(self.rows, self.name)
+        else:
+            ...
+        
+    
+
+# creation of data from the programm itself
 def data_builder():
     data = []
     while True:
@@ -32,9 +75,10 @@ def data_builder():
                 sleep(0.6)
                 try:
                     if raw_values == '':
-                        return My_data_class(data)
+                        data_name= input('Input the name of the data without format:\n')
+                        return My_data_class(data, data_name)
                     else:
-                        values=raw_values.split()
+                        values=[i.strip() for i in raw_values.split()]
                         assert len(values)==len(list_of_titles), "The amount of values does not correspond with the amount of titles!"
                         data.append([np.nan if i=='None' else i for i in values])
                 except AssertionError as err:
@@ -42,5 +86,13 @@ def data_builder():
         except AssertionError as err:
             print(err)
         
-test1=data_builder()
-print(test1.main_data.to_string(index=False))
+# testing data_builder()
+# test1=data_builder()
+# print(test1)
+
+
+# testing saving data
+# test2=data_builder()
+# test2.save_table('csv', 0)
+# test2.save_table('pickle', 0)
+# test2.save_table('txt', 0)
